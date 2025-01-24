6718571ef357b55ce168be9d204dce3f8b30dbdb
Dhavan <dhavan@protonmail.com>
2025-01-24 12:00:48 +0530

    Refactor server initialization to use AppConfig and move database setup
    - Updated `Server::new` to take an `AppConfig` directly instead of individual parameters
    - Moved database initialization to a method within the `Server` struct
    - Simplified server creation by passing the entire configuration object
    - Adjusted `main.rs` to pass the full configuration to `Server::new`

--- src/server.rs
+++ src/server.rs
@@ -2,38 +2,37 @@ use tokio::net::TcpListener;
 use tokio::signal;
 use std::sync::Arc;
 use crate::{handle_connection, store::redis::Store, store::datatype::DataType};
+use crate::config::AppConfig;
 
 pub struct Server {
     listener: TcpListener,
-    // address: String,
-    // port: u16,
     store: Arc<Store>,
+    config: AppConfig,
 }
 
 impl Server {
-    pub async fn new(address: String, port: u16, dir: String, dbfilename: String) -> Result<Self, Box<dyn std::error::Error>> {
+    pub async fn new(config: AppConfig) -> Result<Self, Box<dyn std::error::Error>> {
         let server = Self {
-            listener: TcpListener::bind(format!("{}:{}", address, port)).await?,
-            // address,
-            // port,
+            listener: TcpListener::bind(format!("{}:{}", config.server.address, config.server.port)).await?,
             store: Arc::new(Store::new().await?),
+            config,
         };
-        
-        // Initialize the database
-        Self::init_db(&server.store, dir, dbfilename).await?;
 
         Ok(server)
     }
 
-    async fn init_db(store: &Arc<Store>, dir: String, dbfilename: String) -> Result<(), Box<dyn std::error::Error>> {
+    async fn init_db(&self) -> Result<(), Box<dyn std::error::Error>> {
         // Store config in RAM
-        store.set("dir", DataType::String(dir)).await?;
-        store.set("dbfilename", DataType::String(dbfilename)).await?;
+        self.store.set("dir", DataType::String(self.config.dir.clone())).await?;
+        self.store.set("dbfilename", DataType::String(self.config.dbfilename.clone())).await?;
 
         Ok(())
     }
 
     pub async fn start(&self) -> Result<(), Box<dyn std::error::Error>> {
+        // Initialize the database
+        Self::init_db(&self).await?;
+
         loop {
             tokio::select! {
                 result = self.listener.accept() => {
